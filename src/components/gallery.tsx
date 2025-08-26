299350c4dd3c34c022a3bf5acc3c7515b328394a
Arati Bishwakarma <aratiboswa@gmail.com>
2025-08-26 10:55:53 +0000

    mixed up photos change the photo of "guiding the next generation with wi

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -9,13 +9,13 @@ import {
 
 const galleryItems = [
   {
-    src: '/gallery/bhanteji1.jpg',
+    src: '/gallery/bhanteji3.jpg',
     alt: 'Our Founder, Venerable Dhamma Dhiroo',
     aiHint: 'monk portrait',
     caption: 'founder Venerable Dhamma Dhiroo',
   },
   {
-    src: '/gallery/bhanteji3.jpg',
+    src: '/gallery/bhanteji1.jpg',
     alt: 'Venerable Dhammaviriyo Mahathero with children',
     aiHint: 'monk children',
     caption: 'Guiding the next generation with wisdom.',
