dfa02b3005a07c3e37375df1cb642b3a0dab5325
Arati Bishwakarma <aratiboswa@gmail.com>
2025-08-26 09:31:48 +0000

    no no not all. i meant just the one block haha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import GetInvolved from '@/components/get-involved';
 import HeroSection from '@/components/hero-section';
 import LatestNews from '@/components/latest-news';
 import MissionOverview from '@/components/mission-overview';
+import Gallery from '@/components/gallery';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection />
       <MissionOverview />
+      <Gallery />
       <LatestNews />
       <GetInvolved />
     </div>
